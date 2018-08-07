a5bd67495ff5c062809aaf983ef29b98e066ecc2
mp107 <mp107@o2.pl>
2018-08-07 14:08:34 +0200

    plugins.mrtcommk: Add support for Macedonian live channels streams on mrt.com.mk

---
+++ src/streamlink/plugins/mrt_com_mk.py
@@ -0,0 +1,38 @@
+import logging
+import re
+
+from streamlink.compat import urlparse
+from streamlink.plugin import Plugin
+from streamlink.plugin.api.utils import itertags
+from streamlink.stream import HLSStream
+
+log = logging.getLogger(__name__)
+
+class MrtComMk(Plugin):
+    """
+    Support for Macedonian live channels streams on mrt.com.mk
+    """
+    url_re = re.compile(r'https?://play\.mrt\.com\.mk/live/\d+$')
+
+    @classmethod
+    def can_handle_url(cls, url):
+        return cls.url_re.match(url) is not None
+
+    def _get_streams(self):
+        stream_url = None
+        res = self.session.http.get(self.url)
+
+        for source in itertags(res.text, "source"):
+            if source.attributes.get("src"):
+                stream_url = source.attributes.get("src")
+                url_path = urlparse(stream_url).path
+                if url_path.endswith(".m3u8"):
+                    log.debug("Found stream URL: {0}".format(url_path))
+                    for s in HLSStream.parse_variant_playlist(self.session,
+                                                              stream_url).items():
+                        yield s
+                else:
+                    log.debug("Not used URL path: {0}".format(url_path))
+
+
+__plugin__ = MrtComMk
